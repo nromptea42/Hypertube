1238d2c905f890f0a76d02e104a991288a591f7b
Nathan Rompteaux <nathan.rompteaux@gmail.com>
2017-02-07 18:31:29 +0100

    Ptdr j'ai fais un truc rigolo je sais pas ce que c'est mais sur le tuto c'etait marqué Streaming du coup je pense que j'avance

--- routes/index.js
+++ routes/index.js
@@ -16,18 +16,66 @@ var torrentStream = require('torrent-stream');
 var pump = require('pump');
 var fs = require('fs');
 
-var source = fs.createReadStream('/dev/random');
-var dest = fs.createWriteStream('/dev/null');
-
-// pump(source, dest, function(err) {
-//     console.log('pipe finished', err)
-// });
-//
-// setTimeout(function() {
-//     dest.destroy(); // when dest is closed pump will destroy source
-// }, 1000);
-
 router.get('/', function(req, res, next) {
+    res.render('index', {base_url: 'http://localhost:8000'});
+    // res.json({coucou: "coucou"});
+});
+
+router.get('/video', function (req, res, next) {
+    var file = "./public/videos/film/american/american.mp4";
+    console.log(file);
+
+    fs.stat(file, function (err, stats) {
+        if (err) {
+            if (err.code === 'EONENT')
+                return res.sendStatus(404);
+            return next(err);
+        }
+        var range = req.headers.range;
+
+        if (!range) {
+            var err = new Error("Wrong range");
+                    err.status = 416;
+            return next(err);
+        }
+
+        var positions = range.replace(/bytes=/, "").split("-");
+
+        var start = parseInt(positions[0], 10);
+
+        var file_size = stats.size;
+
+        var end = positions[1] ? parseInt(positions[1], 10) : file_size -1;
+
+        var chunksize = (end - start) + 1;
+
+        var head = {
+            "Content-Range": "bytes " + start + "-" + end + "/" + file_size,
+            "Accept-Ranges": "bytes",
+            "Content-Length": chunksize,
+            "Content-Type": "video/mp4"
+        };
+
+        res.writeHead(206, head);
+
+        var stream_position = {
+            start: start,
+            end: end
+        };
+
+        var stream = fs.createReadStream(file, stream_position);
+
+        stream.on("open", function () {
+            stream.pipe(res);
+        });
+
+        stream.on("error", function(err) {
+            return next(err);
+        });
+    });
+});
+
+router.post('/download', function(req, res, next) {
     var engine = torrentStream('magnet:?xt=urn:btih:1AD001509A762AD98DF9B4353EB5E4775E28F244&dn=American+Pastoral+%282016%29+%5B720p%5D+%5BYTS.AG%5D&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337', {
         tmp: '/Volumes/Storage/goinfre/nromptea',
         path: '/Volumes/Storage/goinfre/nromptea/film'
@@ -44,7 +92,6 @@ router.get('/', function(req, res, next) {
             console.log('finished !');
         });
     });
-
     res.render('index');
 });
 
