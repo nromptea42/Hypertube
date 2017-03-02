bb0e9e3dcc661f6359919c4a7719b798fe3dc6d8
Nathan ROMPTEAUX <nromptea@e1r7p14.42.fr>
2017-03-02 15:30:51 +0100

    ok

--- routes/index.js
+++ routes/index.js
@@ -17,112 +17,161 @@ var torrentStream = require('torrent-stream');
 var pump = require('pump');
 var fs = require('fs');
 var jade = require('jade');
-var parseRange = require('range-parser')
 
-// router.get('/', function(req, res, next) {
-//     res.render('index', {base_url: 'http://localhost:8000'});
-// });
+const path = require('path');
+const parseRange = require('range-parser');
+const engine = torrentStream('magnet:?xt=urn:btih:BB43CF1DC5B200BA37679DB96375A8190D933C2E&dn=Big+Hero+6+%282014%29+%5B720p%5D+%5BYTS.AG%5D&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337');
+const getTorrentFile = new Promise(function (resolve, reject) {
+    engine.on('ready', function() {
+        engine.files.forEach(function (file, idx) {
+            const ext = path.extname(file.name).slice(1);
+            if (ext === 'mkv' || ext === 'mp4') {
+                file.ext = ext;
+                resolve(file);
+            }
+        });
+    });
+});
 
-router.get('*', function (req, res, next) {
-    console.log(req.url);
-    if (req.url != "/Guardians.of.the.Galaxy.2014.1080p.BluRay.x264.YIFY.mp4") {
-        console.log("JE SUIS PASSE LA");
+router.get('*', function(req, res, next) {
+    if (req.url != '/Guardians.of.the.Galaxy.2014.1080p.BluRay.x264.YIFY.mp4') {
+        res.setHeader('Content-Type', 'text/html');
+        if (req.method !== 'GET') return res.end();
         var rpath = __dirname + '/../views/index.jade';
         fs.readFile(rpath, 'utf8', function (err, str) {
-            if (err) {
-                throw err;
-            }
-            var fn = jade.compile(str);
-            res.writeHead(200, { "Content-Type": "text/html" });
-            res.write(fn());
-            res.end();
+            var fn = jade.compile(str, { filename: rpath, pretty: true});
+            res.end(fn());
         });
-    }
-    else {
-        engineGo(23).delay(8000).then(function (result) {
-            console.log('engineGo has happened');
-            var filer = '/Volumes/Storage/goinfre/nromptea/Big.Hero.6.2014.720p.BluRay.x264.YIFY.mp4';
-            fs.stat(filer, function (err, stats) {
-                if (err) {
-                    console.log(err);
-                    if (err.code === 'ENOENT') {
-                        // 404 Error if file not found
-                        return res.sendStatus(404);
-                    }
-                    res.end(err);
-                }
-                var range = parseRange(stats.size, req.headers.range);
-                // console.log("range is " + range);
-                if (!range) {
-                    console.log("no range");
-                    // 416 Wrong range
-                    return res.sendStatus(416);
-                }
-                if (range.type === 'bytes') {
-                    // the ranges
-                    range.forEach(function (r) {
-                        // var positions = range.replace(/bytes=/, "").split("-");
-                        var start = r.start;
-                        var total = stats.size;
-                        var end = r.end;
-                        var chunksize = (end - start);
-                        console.log(start + "  " + end);
-
-                        res.writeHead(206, {
-                            "Content-Range": "bytes " + start + "-" + end + "/" + total,
-                            "Accept-Ranges": "bytes",
-                            'Connection': 'keep-alive',
-                            "Content-Length": chunksize,
-                            "Content-Type": "video/mp4"
-                        });
-                        var stream = fs.createReadStream(filer, {start: start, end: end});
-                        console.log("PUMP INCOMING");
-                        pump(stream, res);
-                    })
-                }
-            });
-        })
+    } else {
+        res.setHeader('Accept-Ranges', 'bytes');
+        getTorrentFile.then(function (file) {
+            res.setHeader('Content-Length', file.length);
+            res.setHeader('Content-Type', `video/${file.ext}`);
+            const ranges = parseRange(file.length, req.headers.range, { combine: true });
+            if (ranges === -1) {
+                // 416 Requested Range Not Satisfiable
+                res.statusCode = 416;
+                return res.end();
+            } else if (ranges === -2 || ranges.type !== 'bytes' || ranges.length > 1) {
+                // 200 OK requested range malformed or multiple ranges requested, stream entire video
+                if (req.method !== 'GET') return res.end();
+                return file.createReadStream().pipe(res);
+            } else {
+                // 206 Partial Content valid range requested
+                const range = ranges[0];
+                res.statusCode = 206;
+                res.setHeader('Content-Length', 1 + range.end - range.start);
+                res.setHeader('Content-Range', `bytes ${range.start}-${range.end}/${file.length}`);
+                if (req.method !== 'GET') return res.end();
+                return file.createReadStream(range).pipe(res);
+            }
+        }).catch(function (e) {
+            console.error(e);
+            res.end(e);
+        });
     }
 });
 
-var runningEngines = {};
+// router.get('*', function (req, res, next) {
+//     console.log(req.url);
+//     if (req.url != "/Guardians.of.the.Galaxy.2014.1080p.BluRay.x264.YIFY.mp4") {
+//         console.log("JE SUIS PASSE LA");
+//         var rpath = __dirname + '/../views/index.jade';
+//         fs.readFile(rpath, 'utf8', function (err, str) {
+//             if (err) {
+//                 throw err;
+//             }
+//             var fn = jade.compile(str);
+//             res.writeHead(200, { "Content-Type": "text/html" });
+//             res.write(fn());
+//             res.end();
+//         });
+//     }
+//     else {
+//         engineGo(23).delay(8000).then(function (result) {
+//             console.log('engineGo has happened');
+//             var filer = '/Volumes/Storage/goinfre/nromptea/Big.Hero.6.2014.720p.BluRay.x264.YIFY.mp4';
+//             fs.stat(filer, function (err, stats) {
+//                 if (err) {
+//                     console.log(err);
+//                     if (err.code === 'ENOENT') {
+//                         // 404 Error if file not found
+//                         return res.sendStatus(404);
+//                     }
+//                     res.end(err);
+//                 }
+//                 var range = parseRange(stats.size, req.headers.range);
+//                 // console.log("range is " + range);
+//                 if (!range) {
+//                     console.log("no range");
+//                     // 416 Wrong range
+//                     return res.sendStatus(416);
+//                 }
+//                 if (range.type === 'bytes') {
+//                     // the ranges
+//                     range.forEach(function (r) {
+//                         // var positions = range.replace(/bytes=/, "").split("-");
+//                         var start = r.start;
+//                         var total = stats.size;
+//                         var end = r.end;
+//                         var chunksize = (end - start);
+//                         console.log(start + "  " + end);
+//
+//                         res.writeHead(206, {
+//                             "Content-Range": "bytes " + start + "-" + end + "/" + total,
+//                             "Accept-Ranges": "bytes",
+//                             'Connection': 'keep-alive',
+//                             "Content-Length": chunksize,
+//                             "Content-Type": "video/mp4"
+//                         });
+//                         var stream = fs.createReadStream(filer, {start: start, end: end});
+//                         console.log("PUMP INCOMING");
+//                         pump(stream, res);
+//                     })
+//                 }
+//             });
+//         })
+//     }
+// });
 
-let engineGo = function (id) {
-    return new Promise(function (resolve, reject) {
-        console.log("entering engineGo");
-        if (runningEngines[id] == undefined) {
-            var engine = torrentStream('magnet:?xt=urn:btih:BB43CF1DC5B200BA37679DB96375A8190D933C2E&dn=Big+Hero+6+%282014%29+%5B720p%5D+%5BYTS.AG%5D&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337', {
-                tmp: '/Volumes/Storage/goinfre/nromptea',
-                path: '/Volumes/Storage/goinfre/nromptea/film'
-            });
-            engine.on('ready', function () {
-                // console.log(engine.files);
-                engine.files.forEach(function (file) {
-                    if (file.name.substr(file.name.length - 3) == 'mkv' || file.name.substr(file.name.length - 3) == 'mp4') {
-                        // console.log('filename:', file.name);
-                        var stream = file.createReadStream();
-                        var writable = fs.createWriteStream('public/videos/' + file.name);
-                        console.log("about to write file");
-                        runningEngines[id] = engine;
-                        pump(stream, writable);
-                        engine.on('download', function () {
-                            // console.log(file.name);
-                            console.log(engine.swarm.downloaded / file.length * 100 + "%");
-                            resolve(file);
-                        });
-                    }
-                    // stream is readable stream to containing the file content
-                });
-            });
-        }
-        else {
-            runningEngines[id].files.forEach(function (file) {
-                if (file.name.substr(file.name.length - 3) == 'mkv' || file.name.substr(file.name.length - 3) == 'mp4') {
-                    resolve(file)
-                }
-            })
-        }
-    });
-};
+// var runningEngines = {};
+
+// let engineGo = function (id) {
+//     return new Promise(function (resolve, reject) {
+//         console.log("entering engineGo");
+//         if (runningEngines[id] == undefined) {
+//             var engine = torrentStream('magnet:?xt=urn:btih:BB43CF1DC5B200BA37679DB96375A8190D933C2E&dn=Big+Hero+6+%282014%29+%5B720p%5D+%5BYTS.AG%5D&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fp4p.arenabg.ch%3A1337&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337', {
+//                 tmp: '/Volumes/Storage/goinfre/nromptea',
+//                 path: '/Volumes/Storage/goinfre/nromptea/film'
+//             });
+//             engine.on('ready', function () {
+//                 // console.log(engine.files);
+//                 engine.files.forEach(function (file) {
+//                     if (file.name.substr(file.name.length - 3) == 'mkv' || file.name.substr(file.name.length - 3) == 'mp4') {
+//                         // console.log('filename:', file.name);
+//                         var stream = file.createReadStream();
+//                         var writable = fs.createWriteStream('public/videos/' + file.name);
+//                         console.log("about to write file");
+//                         runningEngines[id] = engine;
+//                         pump(stream, writable);
+//                         engine.on('download', function () {
+//                             // console.log(file.name);
+//                             console.log(engine.swarm.downloaded / file.length * 100 + "%");
+//                             resolve(file);
+//                         });
+//                     }
+//                     // stream is readable stream to containing the file content
+//                 });
+//             });
+//         }
+//         else {
+//             runningEngines[id].files.forEach(function (file) {
+//                 if (file.name.substr(file.name.length - 3) == 'mkv' || file.name.substr(file.name.length - 3) == 'mp4') {
+//                     resolve(file)
+//                 }
+//             })
+//         }
+//     });
+// };
 
 module.exports = router;
